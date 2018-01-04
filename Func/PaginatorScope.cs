4b3715f73223c950cdc548b1487eb2b71dc5e365
oleksandr dubyna <oleksandr.dubyna@avanade.com>
2018-01-04 10:56:19 +0200

    GetDataToShow to FP

--- Func/PaginatorScope.cs
+++ Func/PaginatorScope.cs
@@ -6,8 +6,45 @@ using System.Threading.Tasks;
 
 namespace Func
 {
-    public class PaginatorScope
+    public static class PaginatorScope
     {
-	//added changes
+        public static Func<int, int, IEnumerable<int>, IEnumerable<int>> GetItemsToShow(
+            Func<int, int, int> getStartIndex,
+            Func<int, int, int> getEndIndex,
+            Func<int, int, IEnumerable<int>, IEnumerable<int>> GetDataStartEndIndex
+            )
+        {
+            return (currentPage, itemsPerPage, DbData) =>
+            {
+                var startIndex = getStartIndex(currentPage, itemsPerPage);
+                var endIndex = getEndIndex(currentPage, itemsPerPage);
+
+                return GetDataStartEndIndex(startIndex, endIndex, DbData);
+            };
+        }
+
+        public static Func<int, int, int> GetStartIndex()
+        {
+            return (CurrentPage, ItemsPerPage) =>
+            {
+                return (CurrentPage - 1) * ItemsPerPage;
+            };
+        }
+
+        public static Func<int, int, int> GetEndIndex()
+        {
+            return (CurrentPage, ItemsPerPage) =>
+            {
+                return CurrentPage * ItemsPerPage;
+            };
+        }
+
+        public static Func<int, int, IEnumerable<int>, IEnumerable<int>> GetDataStartEndIndex()
+        {
+            return (startIndex, endIndex, DbData) =>
+            {
+                return DbData.Where(z => z > startIndex && z <= endIndex);
+            };
+        }
     }
 }
