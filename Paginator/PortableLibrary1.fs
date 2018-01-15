d1e3749698382a27dadff1b80f13079895fb73ae
oleksandr dubyna <oleksandr.dubyna@avanade.com>
2018-01-15 16:41:42 +0200

    F# added

---
+++ Paginator/PortableLibrary1.fs
@@ -0,0 +1,12 @@
+﻿namespace Paginator
+
+exception InnerError of string
+
+type IntMore0Less65535Exclsv(pValue) = 
+    
+    member this.Value = 
+        if pValue > 0 || pValue< 65535
+        then pValue
+        else raise(InnerError("qwe"))
+        
+
