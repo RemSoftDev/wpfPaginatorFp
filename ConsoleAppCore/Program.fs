ca2869badf75f0bee00e4bbc77a3c2c1606efffc
oleksandr dubyna <oleksandr.dubyna@avanade.com>
2018-01-17 10:54:36 +0200

    added console core f# app for debugging f# library

---
+++ ConsoleAppCore/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Paginator.PaginatorScope
+open Paginator.CustomTypes
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let sdf = Init( IntMore0Less65535Exclsv 1,  IntMore0Less65535Exclsv 2, IntMore0Less65535Exclsv 3, [0..110])
+    let sdfa = sdf|>NextState
+    let vc = sdfa.NumberOfPages.Value
+    0 // return an integer exit code
